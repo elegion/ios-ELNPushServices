c3632947ff066e36e8c2c75ddf8fcd32257dcbc6
chebur <chebur.mail@gmail.com>
2016-03-30 20:27:49 +0300

    Fix memory management for properties

--- Source/ELNAPSNotification.h
+++ Source/ELNAPSNotification.h
@@ -23,23 +23,23 @@ NS_ASSUME_NONNULL_BEGIN
 /// Alternatively, you can specify a dictionary as the value of alert.
 ///
 /// If you want the device to display the message text as-is in an alert that has both the Close and View buttons, then specify a string as the direct value of alert. Don’t specify a dictionary as the value of alert if the dictionary only has the body property.
-@property (nonatomic, strong, nullable) ELNAPSNotificationAlert *alert;
+@property (nonatomic, copy, nullable) ELNAPSNotificationAlert *alert;
 
 /// The number to display as the badge of the app icon.
 ///
 /// If this property is absent, the badge is not changed. To remove the badge, set the value of this property to 0.
-@property (nonatomic, strong, nullable) NSNumber *badge;
+@property (nonatomic, copy, nullable) NSNumber *badge;
 
 /// The name of a sound file in the app bundle. The sound in this file is played as an alert. If the sound file doesn’t exist or default is specified as the value, the default alert sound is played. The audio must be in one of the audio data formats that are compatible with system sounds
-@property (nonatomic, strong, nullable) NSString *sound;
+@property (nonatomic, copy, nullable) NSString *sound;
 
 /// Provide this key with a value of 1 to indicate that new content is available. Including this key and value means that when your app is launched in the background or resumed, application:didReceiveRemoteNotification:fetchCompletionHandler: is called.
-@property (nonatomic, strong, nullable) NSNumber *contentAvailable;
+@property (nonatomic, copy, nullable) NSNumber *contentAvailable;
 
 /// Provide this key with a string value that represents the identifier property of the UIMutableUserNotificationCategory object you created to define custom actions. To learn more about using custom actions, see Registering Your Actionable Notification Types.
-@property (nonatomic, strong, nullable) NSString *category;
+@property (nonatomic, copy, nullable) NSString *category;
 
-@property (nonatomic, strong, nullable) NSDictionary *userInfo;
+@property (nonatomic, copy, nullable) NSDictionary *userInfo;
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
