415357862798964e091364aff71c124d5bcd5ace
chebur <chebur.mail@gmail.com>
2016-04-22 14:25:58 +0300

    Add `application did finish launching notification` handler

--- Source/ELNAPSEventsHandlerChain.m
+++ Source/ELNAPSEventsHandlerChain.m
@@ -11,10 +11,17 @@
 @interface ELNAPSEventsHandlerChain ()
 
 @property (nonatomic, strong) NSMutableArray<id<ELNAPSNotificationHandler>> *notificationHandlers;
+@property (nonatomic, assign) BOOL observeApplicationDidFinishLaunchingNotification;
 
 @end
 
 @implementation ELNAPSEventsHandlerChain
+
+#pragma mark - Object Lifecycle
+
+- (void)dealloc {
+    self.observeApplicationDidFinishLaunchingNotification = NO;
+}
 
 #pragma mark - Initialization
 
@@ -22,11 +29,12 @@
     self = [super init];
     if (self) {
         self.notificationHandlers = [NSMutableArray new];
+        self.observeApplicationDidFinishLaunchingNotification = YES;
     }
     return self;
 }
 
-#pragma mark - Notification Handlers
+#pragma mark - Notification Handling
 
 - (void)addNotificationHandler:(id<ELNAPSNotificationHandler>)handler {
     [self.notificationHandlers addObject:handler];
@@ -34,6 +42,49 @@
 
 - (void)removeNotificationHandler:(id<ELNAPSNotificationHandler>)handler {
     [self.notificationHandlers removeObject:handler];
+}
+
+- (void)handleRemoteNotification:(NSDictionary *)userInfo forApplication:(UIApplication *)application fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))handler {
+    ELNAPSNotification *notification = [[ELNAPSNotification alloc] initWithDictionary:userInfo];
+
+    UIBackgroundFetchResult result = UIBackgroundFetchResultNoData;
+    for (id<ELNAPSNotificationHandler> object in self.notificationHandlers) {
+        if ([object shouldHandleNotification:notification forApplication:application]) {
+            result = [object handleNotification:notification forApplication:application];
+            break;
+        }
+    }
+    
+    if (handler) {
+        handler(result);
+    }
+}
+
+#pragma mark - Application Notifications Observation
+
+- (void)setObserveApplicationDidFinishLaunchingNotification:(BOOL)observeApplicationDidFinishLaunchingNotification {
+    if (_observeApplicationDidFinishLaunchingNotification == observeApplicationDidFinishLaunchingNotification) {
+        return;
+    }
+    _observeApplicationDidFinishLaunchingNotification = observeApplicationDidFinishLaunchingNotification;
+    
+    if (self.observeApplicationDidFinishLaunchingNotification) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidFinishLaunching:) name:UIApplicationDidFinishLaunchingNotification object:nil];
+    } else {
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidFinishLaunchingNotification object:nil];
+    }
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    NSDictionary *dictionary = notification.userInfo[UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (dictionary == nil) {
+        return;
+    }
+
+    UIApplication *application = notification.object;
+    [self handleRemoteNotification:dictionary forApplication:application fetchCompletionHandler:^(UIBackgroundFetchResult result) {
+        // do nothing
+    }];
 }
 
 #pragma mark - Delegate Callbacks
@@ -53,19 +104,7 @@
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))handler {
-    ELNAPSNotification *notification = [[ELNAPSNotification alloc] initWithDictionary:userInfo];
-    
-    UIBackgroundFetchResult result = UIBackgroundFetchResultNoData;
-    for (id<ELNAPSNotificationHandler> object in self.notificationHandlers) {
-        if ([object shouldHandleNotification:notification forApplication:application]) {
-            result = [object handleNotification:notification forApplication:application];
-            break;
-        }
-    }
-    
-    if (handler) {
-        handler(result);
-    }
+    [self handleRemoteNotification:userInfo forApplication:application fetchCompletionHandler:handler];
 }
 
 @end
